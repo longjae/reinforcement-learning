2f30fa8663ec7d3f6cf2f7419b5ed91893e09086
longjae <75828593+longjae@users.noreply.github.com>
2023-10-13 15:23:25 +0900

    20231013 ex003

---
+++ ex003.py
@@ -0,0 +1,47 @@
+import numpy as np
+import gymnasium as gym
+
+def compute_value_function(policy):
+    num_iterations = 1000
+    threshold = 1e-20
+    gamma = 1.0
+    
+    value_table = np.zeros(env.observation_space.n)
+    
+    for i in range(num_iterations):
+        updated_value_table = np.copy(value_table)
+        
+        for s in range(env.observation_space.n):
+            a = policy[s]
+            
+            value_table[s] = sum([prob * (r + gamma * updated_value_table[s_]) for prob, s_, r, _ in env.P[s][a]])
+        
+        if np.sum(np.fabs(updated_value_table - value_table)) <= threshold:
+            break
+    return value_table
+
+def extract_policy(value_table):
+    gamma = 1.0
+    policy = np.zeros(env.observation_space.n)
+    for s in range(env.observation_space.n):
+        Q_values = [sum([prob * (r + gamma * value_table[s_]) for prob, s_, r, _ in env.P[s][a]]) for a in range(env.action_space.n)]
+        policy[s] = np.argmax(np.array(Q_values))
+        
+    return policy
+
+def policy_iteration(env):
+    num_iterations = 1000
+    policy = np.zeros(env.observation_space.n)
+    for i in range(num_iterations):
+        value_function = compute_value_function(policy)
+        new_policy = extract_policy(value_function)
+        if np.all(policy == new_policy):
+            break
+        policy = new_policy
+    return policy
+
+    
+if __name__ == "__main__":
+    env = gym.make("FrozenLake-v1", render_mode="human", is_slippery=False)
+    optimal_policy = policy_iteration(env)
+    print(optimal_policy)
